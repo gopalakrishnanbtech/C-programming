a731bb1ba51ce136e985bc17753fb181e2547c28
Gopalakrishnan K <38747333+gopalakrishnanbtech@users.noreply.github.com>
2018-04-26 12:42:38 +0530

    Update largestnum.c

--- codekata/largestnum.c
+++ codekata/largestnum.c
@@ -4,22 +4,22 @@ void main()
 {
     int num1, num2, num3;
  
-    printf("Enter the values of num1, num2 and num3\n");
+    printf("Enter the three values \n");
     scanf("%d %d %d", &num1, &num2, &num3);
-    printf("num1 = %d\tnum2 = %d\tnum3 = %d\n", num1, num2, num3);
+    printf("Number1 = %d\tNumber2 = %d\tNumber3 = %d\n", num1, num2, num3);
     if (num1 > num2)
     {
         if (num1 > num3)
         {
-            printf("num1 is the greatest among three \n");
+            printf("Number1 is the greatest among three \n");
         }
         else
         {
-            printf("num3 is the greatest among three \n");
+            printf("Number3 is the greatest among three \n");
         }
     }
     else if (num2 > num3)
-        printf("num2 is the greatest among three \n");
+        printf("Number2 is the greatest among three \n");
     else
-        printf("num3 is the greatest among three \n");
+        printf("Number3 is the greatest among three \n");
 }
